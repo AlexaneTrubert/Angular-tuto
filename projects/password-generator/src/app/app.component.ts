b3715b156c9a07c8f47b5cb6f86c7f707605ba4c
Alexane <trubert.alexane@orange.fr>
2023-08-14 11:08:18 +0200

    Initialisation du projet password generator
    HTML + CSS + premières variables

---
+++ projects/password-generator/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  message = 'Cliquez sur le bouton "Générer"';
+  length= 20;
+  uppercase = false;
+  numbers = false;
+  symbols = false;
+
+  onClickGenerate() {
+    this.message = 'Mot de passe généré';
+    console.log("Génération du mot de passe avec :");
+    console.table({
+      uppercase: this.uppercase,
+      numbers: this.numbers,
+      symbols: this.symbols,
+      length: this.length
+    })
+  }
+
+  onChangeLength(event: Event) {
+    this.length = +(event.target as HTMLInputElement).value;
+  }
+
+  onChangeSettings(settingName: string, settingValue: boolean) {
+    if (settingName !== 'uppercase' && settingName !== 'numbers' && settingName !== 'symbols') {
+      return
+    }
+    this[settingName] = settingValue;
+  }
+}
