82f65a9f35c106a2cce0dd1804a7c0feacd4d881
Alexane <trubert.alexane@orange.fr>
2023-10-07 09:09:20 +0200

    Tests directives

--- projects/playground/src/app/directives-attributs/highlight.directive.ts
+++ projects/playground/src/app/directives-attributs/highlight.directive.ts
@@ -1,13 +1,17 @@
-import {Directive, HostBinding, HostListener, Input} from "@angular/core";
+import {Directive, EventEmitter, HostBinding, HostListener, Input, Output} from "@angular/core";
 
 @Directive({
   selector: 'p[highlight]',
+  exportAs: 'hl',
 })
 
 export class HighlightDirective
 {
   @HostBinding('style.backgroundColor')
   color = 'transparent';
+
+  @Output('color-change')
+  colorChangeEvent = new EventEmitter<string>();
 
   ngOnInit(){
     this.color = this.baseColor;
@@ -22,11 +26,13 @@ export class HighlightDirective
   @HostListener('mouseenter')
   onMouseEnter(){
     this.color = this.backgroundColor;
+    this.colorChangeEvent.emit(this.color);
   }
 
   @HostListener('mouseout')
   onMouseOut(){
     this.color = this.baseColor;
+    this.colorChangeEvent.emit(this.color);
   }
 
 }
