58f6fc4a60d992df4566343cbb1a0677ee0cd91a
Alexane <trubert.alexane@orange.fr>
2023-10-11 09:54:45 +0200

    Découpage en component du password-generator

---
+++ projects/password-generator/src/app/components/password-controls.component.ts
@@ -0,0 +1,16 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'password-controls',
+  template: `
+    <button (click)="onClickGenerate()">Générer</button>`,
+  styles: []
+})
+export class PasswordControlsComponent {
+  @Output('generate')
+  onGenerateEvent = new EventEmitter();
+
+  onClickGenerate() {
+    this.onGenerateEvent.emit();
+  }
+}
